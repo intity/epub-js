8bd9f18035a2f0be2b17db820d8b3559fcbb5ddf
vite <nuget@list.ru>
2024-07-17 23:10:45 +0300

    updating types/pagelist.d.ts

--- types/pagelist.d.ts
+++ types/pagelist.d.ts
@@ -5,19 +5,20 @@ export interface PageListItem {
     packageUrl?: string
 }
 
-export default class Pagelist {
+export default class Pagelist extends Array {
 
     constructor(xml: XMLDocument);
 
     parse(xml: XMLDocument): PageListItem[];
-    pageFromCfi(cfi: string): number;
     cfiFromPage(pg: string | number): string;
+    pageFromCfi(cfi: string): number;
     pageFromPercentage(percent: number): number;
     percentageFromPage(pg: number): number;
     destroy(): void;
 
-    private item(node: Node): PageListItem;
+    private navItem(node: Node): PageListItem;
+    private ncxItem(node: Node): PageListItem;
     private parseNav(node: Node): PageListItem[];
     private parseNcx(node: Node): PageListItem[];
-    private process(pageList: PageListItem[]): void;
+    private process(): void;
 }
