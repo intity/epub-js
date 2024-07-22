d62e7d8065b9aec9b190094e31cc3a50cf5fd696
vite <nuget@list.ru>
2024-07-22 21:17:24 +0300

    updating test/navigation.js

--- test/navigation.js
+++ test/navigation.js
@@ -3,33 +3,40 @@ import Navigation from "../src/navigation"
 import request from "../src/utils/request"
 
 describe("Navigation", () => {
-    let navigation
+    let nav
     before(() => {
-        navigation = new Navigation()
+        nav = new Navigation()
     })
     describe("#parse()", () => {
         it("should parse navigation from nav.xhtml document", async () => {
-            const nav = await request("/assets/alice/OPS/nav.xhtml")
-            await navigation.parse(nav)
-            assert.equal(navigation.landmarks.size, 1)
-            assert.equal(navigation.pageList.length, 48)
-            assert.equal(navigation.toc.links.size, 11)
-            navigation.clear()
+            const nav = new Navigation()
+            const req = await request("/assets/alice/OPS/nav.xhtml")
+            await nav.parse(req)
+            assert.equal(nav.landmarks.size, 1)
+            assert.equal(nav.pageList.length, 48)
+            assert.equal(nav.toc.links.size, 11)
         })
         it("should parse navigation from nav.ncx document", async () => {
-            const nav = await request("/assets/alice/OPS/nav.ncx")
-            await navigation.parse(nav)
-            assert.equal(navigation.pageList.length, 48)
-            assert.equal(navigation.toc.links.size, 11)
-            navigation.clear()
+            const nav = new Navigation()
+            const req = await request("/assets/alice/OPS/nav.ncx")
+            await nav.parse(req)
+            assert.equal(nav.pageList.length, 48)
+            assert.equal(nav.toc.links.size, 11)
         })
         it("should parse navigation from nav.json object", async () => {
-            const nav = await request("/assets/alice/OPS/nav.json")
-            await navigation.parse(nav)
-            assert.equal(navigation.landmarks.size, 1)
-            assert.equal(navigation.pageList.length, 48)
-            assert.equal(navigation.toc.links.size, 11)
-            navigation.clear()
+            const req = await request("/assets/alice/OPS/nav.json")
+            await nav.parse(req)
+            assert.equal(nav.landmarks.size, 1)
+            assert.equal(nav.pageList.length, 48)
+            assert.equal(nav.toc.links.size, 11)
+        })
+    })
+    describe("#clear()", () => {
+        it("should clear navigation parts", () => {
+            nav.clear()
+            assert.equal(nav.landmarks.size, 0)
+            assert.equal(nav.pageList.length, 0)
+            assert.equal(nav.toc.length, 0)
         })
     })
 })
