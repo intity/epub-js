7e4ce330e4c30fa9ee7a2e7befcd330c87bd41bf
vite <nuget@list.ru>
2024-07-22 20:47:00 +0300

    updating test/navigation.js

--- test/navigation.js
+++ test/navigation.js
@@ -14,12 +14,14 @@ describe("Navigation", () => {
             assert.equal(navigation.landmarks.size, 1)
             assert.equal(navigation.pageList.length, 48)
             assert.equal(navigation.toc.links.size, 11)
+            navigation.clear()
         })
         it("should parse navigation from nav.ncx document", async () => {
             const nav = await request("/assets/alice/OPS/nav.ncx")
             await navigation.parse(nav)
             assert.equal(navigation.pageList.length, 48)
             assert.equal(navigation.toc.links.size, 11)
+            navigation.clear()
         })
         it("should parse navigation from nav.json object", async () => {
             const nav = await request("/assets/alice/OPS/nav.json")
@@ -27,6 +29,7 @@ describe("Navigation", () => {
             assert.equal(navigation.landmarks.size, 1)
             assert.equal(navigation.pageList.length, 48)
             assert.equal(navigation.toc.links.size, 11)
+            navigation.clear()
         })
     })
 })
