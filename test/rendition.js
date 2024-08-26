766de0dd109b23965a788ab7d28e07723fa6974b
vite <nuget@list.ru>
2024-08-26 23:54:04 +0300

    fixed test/rendition.js

--- test/rendition.js
+++ test/rendition.js
@@ -12,45 +12,40 @@ describe("Rendition", () => {
         })
     })
     describe("#display()", () => {
-        it("should be displayed by default", () => {
-            rendition.display().then((section) => {
-                assert.equal(section.index, 0)
-                assert.equal(section.idref, "s0")
-                assert.equal(section.href, "xhtml/nav.xhtml")
-                assert.equal(section.url, "/assets/handbook/EPUB/xhtml/nav.xhtml")
-            })
+        it("should be displayed by default", async () => {
+            const section = await rendition.display()
+            assert.equal(section.index, 0)
+            assert.equal(section.idref, "s0")
+            assert.equal(section.href, "xhtml/nav.xhtml")
+            assert.equal(section.url, "/assets/handbook/EPUB/xhtml/nav.xhtml")
         })
-        it("should be displayed by index", () => {
-            rendition.display(3).then((section) => {
-                assert.equal(section.index, 3)
-                assert.equal(section.idref, "s2")
-                assert.equal(section.href, "xhtml/mathml.xhtml")
-                assert.equal(section.url, "/assets/handbook/EPUB/xhtml/mathml.xhtml")
-            })
+        it("should be displayed by index", async () => {
+            const section = await rendition.display(2)
+            assert.equal(section.index, 2)
+            assert.equal(section.idref, "s2")
+            assert.equal(section.href, "xhtml/mathml.xhtml")
+            assert.equal(section.url, "/assets/handbook/EPUB/xhtml/mathml.xhtml")
         })
-        it("should be displayed by idref", () => {
-            rendition.display("#s2").then((section) => {
-                assert.equal(section.index, 3)
-                assert.equal(section.idref, "s2")
-                assert.equal(section.href, "xhtml/mathml.xhtml")
-                assert.equal(section.url, "/assets/handbook/EPUB/xhtml/mathml.xhtml")
-            })
+        it("should be displayed by idref", async () => {
+            const section = await rendition.display("#s2")
+            assert.equal(section.index, 2)
+            assert.equal(section.idref, "s2")
+            assert.equal(section.href, "xhtml/mathml.xhtml")
+            assert.equal(section.url, "/assets/handbook/EPUB/xhtml/mathml.xhtml")
         })
-        it("should be displayed by href", () => {
-            rendition.display("xhtml/mathml.xhtml").then((section) => {
-                assert.equal(section.index, 3)
-                assert.equal(section.idref, "s2")
-                assert.equal(section.href, "xhtml/mathml.xhtml")
-                assert.equal(section.url, "/assets/handbook/EPUB/xhtml/mathml.xhtml")
-            })
+        it("should be displayed by href", async () => {
+            const section = await rendition.display("xhtml/mathml.xhtml")
+            assert.equal(section.index, 2)
+            assert.equal(section.idref, "s2")
+            assert.equal(section.href, "xhtml/mathml.xhtml")
+            assert.equal(section.url, "/assets/handbook/EPUB/xhtml/mathml.xhtml")
         })
-        it("should be displayed by EpubCFI", () => {
-            rendition.display("epubcfi(/6/6!/4/2[mathml]/2/1:0)").then((section) => {
-                assert.equal(section.index, 3)
-                assert.equal(section.idref, "s2")
-                assert.equal(section.href, "xhtml/mathml.xhtml")
-                assert.equal(section.url, "/assets/handbook/EPUB/xhtml/mathml.xhtml")
-            })
+        it("should be displayed by EpubCFI", async () => {
+            const section = await rendition.display("epubcfi(/6/6!/4/2[mathml]/2/1:0)")
+            assert.equal(section.index, 2)
+            assert.equal(section.idref, "s2")
+            assert.equal(section.href, "xhtml/mathml.xhtml")
+            assert.equal(section.url, "/assets/handbook/EPUB/xhtml/mathml.xhtml")
         })
     })
 })
