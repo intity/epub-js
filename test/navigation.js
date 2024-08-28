cfdd84ee1a88273e02f14ec9aef79c737a3da473
vite <nuget@list.ru>
2024-08-28 16:06:28 +0300

    updating test/navigation.js

--- test/navigation.js
+++ test/navigation.js
@@ -3,29 +3,42 @@ import Navigation from "../src/navigation"
 import request from "../src/utils/request"
 
 describe("Navigation", () => {
-    let nav
-    before(() => {
-        nav = new Navigation()
+    const items = {}
+    before(async () => {
+        const tasks = []
+        const task = async (index, path) => {
+            return request(path).then((doc) => {
+                items[index] = {
+                    nav: new Navigation(),
+                    doc
+                }
+            })
+        }
+        tasks.push(task(0, "/assets/alice/OPS/nav.xhtml"))
+        tasks.push(task(1, "/assets/alice/OPS/nav.ncx"))
+        tasks.push(task(2, "/assets/alice/OPS/nav.json"))
+        return Promise.all(tasks)
     })
     describe("#parse()", () => {
         it("should parse navigation from nav.xhtml document", async () => {
-            const nav = new Navigation()
-            const req = await request("/assets/alice/OPS/nav.xhtml")
-            await nav.parse(req)
+            const nav = items[0].nav
+            const doc = items[0].doc
+            await nav.parse(doc)
             assert.equal(nav.landmarks.size, 1)
             assert.equal(nav.pageList.length, 48)
             assert.equal(nav.toc.links.size, 11)
         })
         it("should parse navigation from nav.ncx document", async () => {
-            const nav = new Navigation()
-            const req = await request("/assets/alice/OPS/nav.ncx")
-            await nav.parse(req)
+            const nav = items[1].nav
+            const doc = items[1].doc
+            await nav.parse(doc)
             assert.equal(nav.pageList.length, 48)
             assert.equal(nav.toc.links.size, 11)
         })
         it("should parse navigation from nav.json object", async () => {
-            const req = await request("/assets/alice/OPS/nav.json")
-            await nav.parse(req)
+            const nav = items[2].nav
+            const doc = items[2].doc
+            await nav.parse(doc)
             assert.equal(nav.landmarks.size, 1)
             assert.equal(nav.pageList.length, 48)
             assert.equal(nav.toc.links.size, 11)
@@ -33,6 +46,7 @@ describe("Navigation", () => {
     })
     describe("#clear()", () => {
         it("should clear navigation parts", () => {
+            const nav = items[0].nav
             nav.clear()
             assert.equal(nav.landmarks.size, 0)
             assert.equal(nav.pageList.length, 0)
