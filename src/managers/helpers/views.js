1b6e47f275b9eb2c91714862c2f57baec6d60133
vite <nuget@list.ru>
2024-09-26 20:42:40 +0300

    refactoring src/managers/helpers/views.js - removed views.hidden property - replaced views.destroy method - added views.init method

--- src/managers/helpers/views.js
+++ src/managers/helpers/views.js
@@ -13,17 +13,17 @@ class Views extends Array {
 		 * @memberof Views
 		 * @readonly
 		 */
+		this.container = null;
+		this.init();
+	}
+
+	init() {
+
 		this.container = document.createElement("div");
 		this.container.classList.add("views-container");
 		this.container.style.display = "flex";
 		this.container.style.width = "100%";
 		this.container.style.height = "100%";
-		/**
-		 * @member {boolean} hidden
-		 * @memberof Views
-		 * @readonly
-		 */
-		this.hidden = false;
 	}
 
 	/**
@@ -99,40 +99,29 @@ class Views extends Array {
 	/**
 	 * remove
 	 * @param {object} view 
+	 * @param {number} [i] index
 	 */
-	remove(view) {
-
-		const index = this.indexOf(view);
+	remove(view, i) {
+		
+		const index = i || this.indexOf(view);
 		if (index > -1) {
+			this.container.removeChild(view.element);
 			this.splice(index, 1);
-		}
-		this.destroy(view);
-	}
-
-	/**
-	 * destroy
-	 * @param {object} view 
-	 */
-	destroy(view) {
-
-		if (view.displayed) {
 			view.destroy();
 		}
-		this.container.removeChild(view.element);
 	}
 
 	/**
 	 * clear
-	 * @returns {void}
 	 */
 	clear() {
 
-		if (this.length === 0) return;
-		for (let i = 0; i < this.length; i++) {
+		const len = this.length;
+
+		for (let i = 0; i < len; ++i) {
 			const view = this[i];
-			this.destroy(view);
+			this.remove(view, i);
 		}
-		this.splice(0);
 	}
 
 	/**
@@ -144,7 +133,7 @@ class Views extends Array {
 
 		for (let i = 0; i < this.length; i++) {
 			const view = this[i];
-			if (view.displayed && 
+			if (view.displayed &&
 				view.section.index == section.index) {
 				return view;
 			}
@@ -178,7 +167,6 @@ class Views extends Array {
 				view.show();
 			}
 		}
-		this.hidden = false;
 	}
 
 	/**
@@ -192,7 +180,6 @@ class Views extends Array {
 				view.hide();
 			}
 		}
-		this.hidden = true;
 	}
 
 	/**
@@ -206,6 +193,15 @@ class Views extends Array {
 				view.update();
 			}
 		}
+	}
+
+	/**
+	 * destroy
+	 */
+	destroy() {
+
+		this.clear();
+		this.container = undefined;
 	}
 }
 
