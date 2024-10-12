fb1bef125cfa559e37f9f42cb2b4e44f2cc6cc52
vite <nuget@list.ru>
2024-10-12 13:10:51 +0300

    updating test/rendition.js

--- test/rendition.js
+++ test/rendition.js
@@ -1,6 +1,8 @@
 import assert from "assert"
 import Book from "../src/book"
 
+const url = () => /epub-js/.test(location.href) ? "/epub-js" : ""
+
 describe("Rendition", () => {
     let book, rendition
     before(async () => {
@@ -17,35 +19,35 @@ describe("Rendition", () => {
             assert.equal(section.index, 0)
             assert.equal(section.idref, "s0")
             assert.equal(section.href, "xhtml/nav.xhtml")
-            assert.equal(section.url, "/assets/handbook/EPUB/xhtml/nav.xhtml")
+            assert.equal(section.url, `${url()}/assets/handbook/EPUB/xhtml/nav.xhtml`)
         })
         it("should be displayed by index", async () => {
             const section = await rendition.display(2)
             assert.equal(section.index, 2)
             assert.equal(section.idref, "s2")
             assert.equal(section.href, "xhtml/mathml.xhtml")
-            assert.equal(section.url, "/assets/handbook/EPUB/xhtml/mathml.xhtml")
+            assert.equal(section.url, `${url()}/assets/handbook/EPUB/xhtml/mathml.xhtml`)
         })
         it("should be displayed by idref", async () => {
             const section = await rendition.display("#s2")
             assert.equal(section.index, 2)
             assert.equal(section.idref, "s2")
             assert.equal(section.href, "xhtml/mathml.xhtml")
-            assert.equal(section.url, "/assets/handbook/EPUB/xhtml/mathml.xhtml")
+            assert.equal(section.url, `${url()}/assets/handbook/EPUB/xhtml/mathml.xhtml`)
         })
         it("should be displayed by href", async () => {
             const section = await rendition.display("xhtml/mathml.xhtml")
             assert.equal(section.index, 2)
             assert.equal(section.idref, "s2")
             assert.equal(section.href, "xhtml/mathml.xhtml")
-            assert.equal(section.url, "/assets/handbook/EPUB/xhtml/mathml.xhtml")
+            assert.equal(section.url, `${url()}/assets/handbook/EPUB/xhtml/mathml.xhtml`)
         })
         it("should be displayed by EpubCFI", async () => {
             const section = await rendition.display("epubcfi(/6/6!/4/2[mathml]/2/1:0)")
             assert.equal(section.index, 2)
             assert.equal(section.idref, "s2")
             assert.equal(section.href, "xhtml/mathml.xhtml")
-            assert.equal(section.url, "/assets/handbook/EPUB/xhtml/mathml.xhtml")
+            assert.equal(section.url, `${url()}/assets/handbook/EPUB/xhtml/mathml.xhtml`)
         })
     })
     after(() => {
