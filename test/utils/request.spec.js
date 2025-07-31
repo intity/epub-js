b6cd236df629a7a9b670ea24759ea4269e815451
vite <nuget@list.ru>
2025-07-31 17:37:54 +0300

    updating test/utils/request.spec.js

--- test/utils/request.spec.js
+++ test/utils/request.spec.js
@@ -1,15 +1,22 @@
 import assert from "assert"
 import request from "../../src/utils/request"
 
+const url = (path) => {
+
+	let result = location.origin
+	if (/localhost/.test(result)) {
+		result += path
+	} else {
+		result += "epub-js"
+		result += path
+	}
+	return result
+}
+
 describe("#request()", () => {
-    it("should be request of the xhtml type", async () => {
-        const uri = "../assets/handbook/EPUB/xhtml/nav.xhtml"
-        const doc = await request(uri, "xhtml").then((e) => {
-			console.log(doc)
-		},(err) => {
-			console.log(err)
-			console.log("TARGET: ", err.target instanceof XMLHttpRequest)
-		})
-		assert.ok(typeof doc === "object")
-    })
+	it("should be request of the xhtml type", async () => {
+		const uri = url("/assets/handbook/EPUB/xhtml/nav.xhtml")
+		const doc = await request(uri, "xhtml")
+		assert.ok(doc instanceof Document)
+	})
 })
