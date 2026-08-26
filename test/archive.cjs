8bb0ae73026d33d7952ab103973234ef350dde1e
vite <nuget@list.ru>
2026-08-26 18:55:02 +0300

    updating test/archive.cjs

--- test/archive.cjs
+++ test/archive.cjs
@@ -15,7 +15,7 @@ describe("Archive", () => {
     const absolutePath = path.resolve(__dirname, "../assets/alice.epub");
     bookPath = pathToFileURL(absolutePath).href;
   });
-  it("should be created #0.instance JSZip", () => {
+  it("should be #0.constructor init (JSZip instance)", () => {
     const archive = new Archive();
     TEST_LIST.push({ archive });
     assert.ok(archive.instance instanceof JSZip);
@@ -23,17 +23,23 @@ describe("Archive", () => {
   it("should be #0.openUrl *.epub file", async () => {
     const { archive } = TEST_LIST[0];
     const jszip = await archive.openUrl(bookPath);
+    const mimetype = jszip.files["mimetype"];
     assert.ok(jszip instanceof JSZip);
-    assert.strictEqual(jszip.files["mimetype"].name, "mimetype");
+    assert.ok(mimetype.date instanceof Date);
+    assert.strictEqual(mimetype.name, "mimetype");
+    assert.strictEqual(mimetype.dir, false);
   });
   it("should be #0.get entry from archive by URI", () => {
     const { archive } = TEST_LIST[0];
     const entry = archive.get("/META-INF/container.xml");
+    assert.ok(entry.date instanceof Date);
     assert.strictEqual(entry.name, CONTAINER_PATH_0);
+    assert.strictEqual(entry.dir, false);
   });
-  it("should be #0.clear instance", async () => {
+  it("should be #0.clear object (JSZip.files empty)", async () => {
     const { archive } = TEST_LIST[0];
     archive.clear();
+    assert.strictEqual(Object.keys(archive.instance.files).length, 0);
     assert.strictEqual(archive.instance.files["mimetype"], undefined);
   });
   it("should be #0.destroy object", () => {
